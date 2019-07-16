4c9a5911a60304e73c84e13557e4f324b2fe0b45
godiman <goodnewsetim30@gmail.com>
2019-07-16 18:18:57 +0100

    changes to the main method

--- java/antOnChessBoard2/src/antonchessboard2/AntOnChessBoard2.java
+++ java/antOnChessBoard2/src/antonchessboard2/AntOnChessBoard2.java
@@ -15,7 +15,7 @@ public class AntOnChessBoard2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Well come");
+        System.out.println("well come to github");
     }
     
 }
