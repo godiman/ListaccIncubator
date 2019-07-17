46e64f7c5df29534ca331108f4409f568fd16701
godiman <goodnewsetim30@gmail.com>
2019-07-17 07:37:08 +0100

    Changes to the main method

--- java/antOnChessBoard2/src/antonchessboard2/AntOnChessBoard2.java
+++ java/antOnChessBoard2/src/antonchessboard2/AntOnChessBoard2.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// new change today
 package antonchessboard2;
 
 /**
@@ -15,7 +11,8 @@ public class AntOnChessBoard2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("well come to github");
+        System.out.println("well come");
+        System.out.println("Github");
     }
     
 }
